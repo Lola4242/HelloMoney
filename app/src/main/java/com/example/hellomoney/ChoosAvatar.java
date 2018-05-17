4a73fd21722e0b5d0b3cf0a66f8e97fb9c9e7f43
Ylona Meeuwenberg <ylona.meeuwenberg@devolksbank.nl>
2018-05-17 19:22:42 +0200

    Chose your avatar

--- app/src/main/java/com/example/hellomoney/ChoosAvatar.java
+++ app/src/main/java/com/example/hellomoney/ChoosAvatar.java
@@ -1,17 +1,82 @@
 package com.example.hellomoney;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.hellomoney.model.Child;
 
 public class ChoosAvatar extends AppCompatActivity {
 
+    public Child child = new Child();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choos_avatar);
-        Child child = (Child) getIntent().getParcelableExtra("test");
-        System.out.println(child.getAvatar());
+    }
+
+    public void child1(View view){
+        Intent intent = new Intent(this, ChoosAvatar.class);
+        child.setAvatar(1);
+        intent.putExtra("child", child);
+        startActivity(intent);
+    }
+
+    public void child2(View view){
+        Intent intent = new Intent(this, ChoosAvatar.class);
+        child.setAvatar(2);
+        intent.putExtra("child", child);
+        startActivity(intent);
+    }
+
+    public void child3(View view){
+        Intent intent = new Intent(this, ChoosAvatar.class);
+        child.setAvatar(3);
+        intent.putExtra("child", child);
+        startActivity(intent);
+    }
+
+    public void child4(View view){
+        Intent intent = new Intent(this, ChoosAvatar.class);
+        child.setAvatar(7);
+        intent.putExtra("child", child);
+        startActivity(intent);
+    }
+
+    public void child5(View view){
+        Intent intent = new Intent(this, ChoosAvatar.class);
+        child.setAvatar(5);
+        intent.putExtra("child", child);
+        startActivity(intent);
+    }
+
+    public void child6(View view){
+        Intent intent = new Intent(this, ChoosAvatar.class);
+        child.setAvatar(6);
+        intent.putExtra("child", child);
+        startActivity(intent);
+    }
+
+    public void child7(View view){
+        Intent intent = new Intent(this, ChoosAvatar.class);
+        child.setAvatar(7);
+        intent.putExtra("child", child);
+        startActivity(intent);
+    }
+
+    public void child8(View view){
+        Intent intent = new Intent(this, ChoosAvatar.class);
+        child.setAvatar(8);
+        intent.putExtra("child", child);
+        startActivity(intent);
+    }
+
+    public void child9(View view){
+        Intent intent = new Intent(this, ChoosAvatar.class);
+        child.setAvatar(9);
+        intent.putExtra("child", child);
+        startActivity(intent);
     }
 }
